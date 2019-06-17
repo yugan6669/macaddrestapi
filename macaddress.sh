89c794407b6ad97b931832a322c765a93c29830c
krishnamaram2 <48556990+krishnamaram2@users.noreply.github.com>
2019-06-17 11:15:54 +0530

    Update macaddress.sh

--- macaddress.sh
+++ macaddress.sh
@@ -3,12 +3,13 @@
 ##########################################################################################
 #!/bin/bash
 arg=``
+count=0
 case $@ in
 $arg)
      echo -e "\n"
      read -p "Please Enter  Your API Key: "  API_KEY
      echo -e "\n"
-     if [ ! -f macaddress.txt ]; then
+     if [ ! -f macaddress.txt ] ||  [ -z "$(cat macaddress.txt)" ]; then
        read -p "Please Enter your MAC Address: "  MAC_ADDRESS
        echo -e "\n \n \n"
        #Build the Image using  Dockerfile
@@ -18,14 +19,18 @@ $arg)
        docker container run  mac_addr $API_KEY $MAC_ADDRESS
        echo -e "\n \n \n"
      else
+         echo -e "Docker Image creation and running the container is under progress...\n"
          for MAC_ADDRESS in `cat macaddress.txt`;
-            do
-            #Build the Image using  Dockerfile
-            echo -e "Docker Image creation and running the container is under progress...\n"
-            docker image  build -t mac_addr . > /dev/null 2>&1
-            #Run the container and pass API_KEY and MAC_ADDRESS as arguments
-            docker container run -it mac_addr $API_KEY $MAC_ADDRESS
-            echo -e "\n \n \n"
+           do
+            count=$((count+1))
+           if [ $count == 1 ]; then
+              docker image build -t mac_addr . > /dev/null 2>&1
+              docker container run -it mac_addr $API_KEY $MAC_ADDRESS
+           else
+              #Run the container and pass API_KEY and MAC_ADDRESS as arguments
+              docker container run -it mac_addr $API_KEY $MAC_ADDRESS
+              echo -e "\n \n \n"
+             fi
             done
      fi
    ;;
